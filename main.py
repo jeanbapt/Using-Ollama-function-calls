f26f6e5abad198dd270e6ffc592cf47a5c8fd0fc
OhZanbat <jeanbapt@dealexmachina.com>
2024-05-25 10:09:57 +0200

    update launch.json

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import json
 import sys
 from haversine import haversine
 
-# the required parameters on the command line
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <country1> <country2>")
+        sys.exit(1)
 country1 = sys.argv[1]
 country2 = sys.argv[2]
 
-# the expected schema of the returned JSON
+
 schema = {
     "capital_city": {
         "type": "string",
@@ -24,18 +27,11 @@ schema = {
 
 }
 
-# using mistral here, but you can use and compare many different models and choose the most appropriate
 model = "mistral"
-# Let's put our countries in a dict
 countries=[]
 countries.append(country1)
 countries.append(country2)
-# Let's initiate the cities_info dict
 cities_info=[]
-
-# This is a function that check the city_info returned by the llm agains the schema, 
-# returns trus while it's all good, False when not. On false, the llm request will be repeated.
-# The type verification could be enhanced with pydantic objects.
 
 def is_valid_city_info(city_info, schema):
     try:
@@ -50,11 +46,6 @@ def is_valid_city_info(city_info, schema):
     except Exception as e:
         print(f"Validation error: {e}")
         return False
-
-# This is a function that takes a country as input - maybe it should be checked against a canonical list of countries. 
-# It calls the ollama model on port 11434 of localhost. This should be factored out as parameters in a config file. 
-# It returns a JSON according to the schema. For that, you can see how to put a straightjacket prompt. Change any of this
-# to measure how wild it can be. 
 
 def get_city_info(country):
     payload = {
@@ -71,11 +62,6 @@ def get_city_info(country):
         "format": "json",
         "stream": False
     }
-
-# Main loop, holding True until proper answers are provided. 
-# Trials shoud be limited as it could go on for ever. 
-# Some unnecessary extra tests are put in, you can take them out once you are pretty sure it works.
-    
     while True:    
         try:
             response = requests.post("http://localhost:11434/api/chat", json=payload)
@@ -97,14 +83,12 @@ def get_city_info(country):
             print(f"Key error: {e}")
             sys.exit(1)
         
-# We have new retrieved city_info , i.e. the name of the capital city and its coordinates,
-# Let go and calculate the distance in km using haversine. Note that the type of the JSON must
-# be respected to perform the operation.
-
+    
 for country in countries:
     city_info = get_city_info(country)
     print(f"Retrieved city info for {country}: {city_info}")
     cities_info.append(city_info)
+
 
 city1 = cities_info[0]
 city2 = cities_info[1]
@@ -119,4 +103,5 @@ distance = haversine(city1_coords, city2_coords)
 
 print(f"The distance between {city1['capital_city']} and {city2['capital_city']} is {int(round(distance, -1))} km")
 
-# Have fun and kick butts
+if __name__ == "__main__":
+    main()
